1143d48bc39481846209e4c8a04b6c6938e0c325
MohammadNasir520 <nasirahsan520@gmail.com>
2023-08-22 17:00:59 +0600

    pagination done

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,4 +1,5 @@
 import { AcademicSemester, PrismaClient } from '@prisma/client';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
 import { IGenericResponse } from '../../../interfaces/common';
 
 const prisma = new PrismaClient();
@@ -13,16 +14,21 @@ const insertIntoDB = async (
   return result;
 };
 
-const getAllFromDB = async (): Promise<
-  IGenericResponse<AcademicSemester[]>
-> => {
-  const result = await prisma.academicSemester.findMany();
+const getAllFromDB = async (
+  filters,
+  options
+): Promise<IGenericResponse<AcademicSemester[]>> => {
+  const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+  const result = await prisma.academicSemester.findMany({
+    skip,
+    take: limit,
+  });
   const total = await prisma.academicSemester.count();
   return {
     meta: {
       total,
-      page: 1,
-      limit: 10,
+      page,
+      limit,
     },
     data: result,
   };
