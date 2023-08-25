d48afe1463f9a3190479842fd68456828cbab522
MohammadNasir520 <nasirahsan520@gmail.com>
2023-08-25 14:20:28 +0600

    building validation added

--- src/app/modules/building/building.routes.ts
+++ src/app/modules/building/building.routes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { buildingController } from './building.controller';
+import { BuildingValidation } from './building.validation';
 
 const router = express.Router();
 
-router.post('/', buildingController.insertIntoDb);
+router.post(
+  '/',
+  validateRequest(BuildingValidation.create),
+  buildingController.insertIntoDb
+);
 router.get('/', buildingController.getAllFromDb);
 
 export const BuildingRoutes = router;
