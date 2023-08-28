23c5b1ca364193cd1c0ef0bb15a167b5eb756292
MohammadNasir520 <nasirahsan520@gmail.com>
2023-08-29 03:12:13 +0600

    room get, update , deltee done

--- src/app/modules/room/room.routes.ts
+++ src/app/modules/room/room.routes.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { RoomController } from './room.controller';
+import { RoomValidation } from './room.validation';
 
 const router = express.Router();
 
@@ -9,6 +11,22 @@ router.post(
   '/',
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   RoomController.insertIntoDb
+);
+
+router.get('/', RoomController.getAllFromDB);
+router.get('/:id', RoomController.getByIdFromDB);
+
+router.patch(
+  '/:id',
+  validateRequest(RoomValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  RoomController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  RoomController.deleteByIdFromDB
 );
 
 export const RoomRoutes = router;
