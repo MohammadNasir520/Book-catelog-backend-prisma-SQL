cba6e3c074da11b1d8e8175470bdb5345fc01970
MohammadNasir520 <nasirahsan520@gmail.com>
2023-09-03 15:44:13 +0600

    semester registration validation done

--- src/app/modules/course/course.validation.ts
+++ src/app/modules/course/course.validation.ts
@@ -17,7 +17,16 @@ const update = z.object({
   }),
 });
 
+const assignOrRemoveFaculties = z.object({
+  body: z.object({
+    faculties: z.array(z.string(), {
+      required_error: 'faculties required',
+    }),
+  }),
+});
+
 export const CourseValidation = {
   create,
   update,
+  assignOrRemoveFaculties,
 };
--- src/app/modules/faculty/faculty.routes.ts
+++ src/app/modules/faculty/faculty.routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { FacultyController } from './faculty.controller';
 import { FacultyValidation } from './faculty.validations';
@@ -15,7 +17,17 @@ router.post(
   FacultyController.insertIntoDB
 );
 
-router.post('/:id/assign-courses', FacultyController.assignCourses);
-router.delete('/:id/remove-courses', FacultyController.removeCourses);
+router.post(
+  '/:id/assign-courses',
+  validateRequest(FacultyValidation.assignOrRemoveCourses),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  FacultyController.assignCourses
+);
+router.delete(
+  '/:id/remove-courses',
+  validateRequest(FacultyValidation.assignOrRemoveCourses),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  FacultyController.removeCourses
+);
 
 export const facultyRoutes = router;
