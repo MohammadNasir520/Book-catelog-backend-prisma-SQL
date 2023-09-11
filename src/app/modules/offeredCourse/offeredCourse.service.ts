4ba9c57f3f768d9e44339da530bbebc247bc3cdf
MohammadNasir520 <nasirahsan520@gmail.com>
2023-09-11 17:19:40 +0600

    offered course added

---
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -0,0 +1,52 @@
+import { OfferedCourse } from '@prisma/client';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
+import prisma from '../../../shared/prisma';
+import { asyncForEach } from '../../../shared/utils';
+import { ICreateOfferedCourse } from './offeredCourse.interface';
+
+const insertIntoDB = async (
+  data: ICreateOfferedCourse
+): Promise<OfferedCourse[]> => {
+  const { academicDepartmentId, semesterRegistrationId, courseIds } = data;
+  const result: OfferedCourse[] = [];
+
+  await asyncForEach(courseIds, async (courseId: string) => {
+    const alreadyExist = await prisma.offeredCourse.findFirst({
+      where: {
+        academicDepartmentId,
+        semesterRegistrationId,
+        courseId,
+      },
+    });
+
+    if (alreadyExist) {
+      throw new ApiError(
+        httpStatus.BAD_REQUEST,
+        'offered course already exist'
+      );
+    }
+    if (!alreadyExist) {
+      const insertOfferedCourse = await prisma.offeredCourse.create({
+        data: {
+          academicDepartmentId,
+          semesterRegistrationId,
+          courseId,
+        },
+        include: {
+          academicDepartment: true,
+          semesterRegistration: true,
+          course: true,
+        },
+      });
+
+      result.push(insertOfferedCourse);
+    }
+  });
+
+  return result;
+};
+
+export const offeredCourseService = {
+  insertIntoDB,
+};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,6 +5,7 @@ import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSeme
 import { BuildingRoutes } from '../modules/building/building.routes';
 import { CourseRoutes } from '../modules/course/course.routes';
 import { facultyRoutes } from '../modules/faculty/faculty.routes';
+import { offeredCourseRoutes } from '../modules/offeredCourse/offeredCourse.routes';
 import { RoomRoutes } from '../modules/room/room.routes';
 import { SemesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.routes';
 import { studentRoutes } from '../modules/student/student.routes';
@@ -48,6 +49,10 @@ const moduleRoutes = [
   {
     path: '/semester-registration',
     route: SemesterRegistrationRoutes,
+  },
+  {
+    path: '/offered-courses',
+    route: offeredCourseRoutes,
   },
 ];
 
