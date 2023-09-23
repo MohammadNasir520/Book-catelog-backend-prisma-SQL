8d165dab73a96df08a82a337936f7a6eae57bdf2
MohammadNasir520 <nasirahsan520@gmail.com>
2023-09-23 21:46:17 +0600

    pagination ok

--- src/app/modules/book/book.controller.ts
+++ src/app/modules/book/book.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
 import { BookService } from './book.service';
 
@@ -15,7 +16,9 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
 });
 
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
-  const result = await BookService.getAllFromDB();
+  // const filters = pick(req.query, ['genre', 'title']);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await BookService.getAllFromDB(options);
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
--- src/app/modules/book/book.service.ts
+++ src/app/modules/book/book.service.ts
@@ -1,4 +1,7 @@
 import { Book } from '@prisma/client';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
 import prisma from '../../../shared/prisma';
 
 const insertIntoDB = async (data: Book): Promise<Book> => {
@@ -11,13 +14,34 @@ const insertIntoDB = async (data: Book): Promise<Book> => {
   return result;
 };
 
-const getAllFromDB = async (): Promise<Book[]> => {
+const getAllFromDB = async (
+  options: IPaginationOptions
+): Promise<IGenericResponse<Book[]>> => {
+  console.log(options);
+
+  const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+
+  console.log(page, limit, skip);
+
   const result = await prisma.book.findMany({
     include: {
       category: true,
     },
+    skip,
+    take: limit,
+    orderBy: {
+      [options.sortBy as string]: options.sortOrder,
+    },
   });
-  return result;
+
+  return {
+    meta: {
+      total: 0,
+      page,
+      limit,
+    },
+    data: result,
+  };
 };
 
 const getByIdFromDB = async (id: string): Promise<Book | null> => {
