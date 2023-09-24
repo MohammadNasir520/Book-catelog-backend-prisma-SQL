58c4d1f9e5136244a2355a4f3096550aa272d50c
MohammadNasir520 <nasirahsan520@gmail.com>
2023-09-24 16:28:12 +0600

    category validation added

--- src/app/modules/category/category.routes.ts
+++ src/app/modules/category/category.routes.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { CategoryController } from './category.controller';
+import { categoryValidation } from './category.validation';
 
 const router = express.Router();
 
 router.post(
   '/create-category',
   auth(ENUM_USER_ROLE.ADMIN),
+  validateRequest(categoryValidation.categoryCreateZodSchema),
   CategoryController.insertIntoDB
 );
 router.get('/', CategoryController.getAllFromDB);
